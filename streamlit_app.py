7647196f548e467093cbf4bb7f7d7d5414b0cd06
rmanasa3 <132876701+rmanasa3@users.noreply.github.com>
2023-05-08 12:07:57 +0530

    Update streamlit_app.py
    Added Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit 
 
-streamlit.title("Hey Snowflake")
+streamlit.title("New Healthy Dinner")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach &Rocket smoothie')
+streamlit.text('Hard-Boiled Free Range')
+
