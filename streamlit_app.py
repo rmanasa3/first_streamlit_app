f3c3c7a4b70bfcaca7ba970fea4e136bbe8438ec
rmanasa3 <132876701+rmanasa3@users.noreply.github.com>
2023-05-08 12:11:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ streamlit.text('🥗 Kale, Spinach &Rocket smoothie')
 streamlit.text('🐔 Hard-Boiled Free Range')
 streamlit.text("🥑🍞 Avocado Toast")
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
